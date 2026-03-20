d73169191b289aa7b6aad874025d5c90719a1782
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2026-03-20 10:09:17 +0300

    feat: add User type and mock data

---
+++ src/mocks/users.ts
@@ -0,0 +1,10 @@
+import type {IUser} from "../types/user.ts";
+
+
+export const MOCKS_USERS: IUser[] = [
+    {id: 1, name: 'Виктор Викторович', username: 'victor_v'},
+    {id: 2, name: 'Ольга Смиронова', username: 'olga_s'},
+    {id: 3, name: 'Анна Прокофьевна', username: 'anna_p'},
+    {id: 4, name: 'Лаврентий Алексеевич', username: 'lavrentii_a'},
+    {id: 5, name: 'Екатерина Кузнецова', username: 'ekaterina_k'},
+]
